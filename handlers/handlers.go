dbd1cbae486f0d6e4844c0f75338ec200f8fa8fe
Amands123 <agraman1994@gmail.com>
2026-05-14 22:02:04 +0530

    modified

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,65 +1,120 @@
 package handlers
 
 import (
-    "html/template"
-    "net/http"
+	"html/template"
+	"net/http"
 )
 
 type Toy struct {
-    ID          int
-    Name        string
-    Price       string
-    Description string
-    Image       string
+	ID          int
+	Name        string
+	Category    string
+	Price       string
+	Description string
+	Image       string
 }
 
 var toys = []Toy{
-    {
-        ID: 1,
-        Name: "Teddy Bear",
-        Price: "$20",
-        Description: "Soft teddy bear for kids",
-        Image: "/static/images/teddy.jpg",
-    },
-    {
-        ID: 2,
-        Name: "Remote Car",
-        Price: "$35",
-        Description: "Battery powered racing car",
-        Image: "/static/images/car.jpg",
-    },
-    {
-        ID: 3,
-        Name: "Robot Toy",
-        Price: "$40",
-        Description: "Smart dancing robot",
-        Image: "/static/images/robot.jpg",
-    },
-    {
-        ID: 4,
-        Name: "LEGO Set",
-        Price: "$55",
-        Description: "Creative building blocks set",
-        Image: "/static/images/lego.jpg",
-    },
+	{
+		ID:          1,
+		Name:        "Teddy Bear",
+		Category:    "Teddy",
+		Price:       "$20",
+		Description: "Soft teddy bear for kids",
+		Image:       "/static/images/teddy.jpg",
+	},
+	{
+		ID:          2,
+		Name:        "Robot Toy",
+		Category:    "Robot",
+		Price:       "$40",
+		Description: "Smart dancing robot",
+		Image:       "/static/images/robot.jpg",
+	},
+	{
+		ID:          3,
+		Name:        "Remote Car",
+		Category:    "Car",
+		Price:       "$35",
+		Description: "Battery powered racing car",
+		Image:       "/static/images/car.jpg",
+	},
+	{
+		ID:          4,
+		Name:        "LEGO Set",
+		Category:    "Lego",
+		Price:       "$55",
+		Description: "Creative building blocks set",
+		Image:       "/static/images/lego.jpg",
+	},
 }
 
+var cart []Toy
+
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-    tmpl := template.Must(template.ParseFiles("templates/home.html"))
-    tmpl.Execute(w, toys)
+
+	tmpl := template.Must(template.ParseFiles("templates/home.html"))
+
+	tmpl.Execute(w, toys)
 }
 
 func ToysHandler(w http.ResponseWriter, r *http.Request) {
-    tmpl := template.Must(template.ParseFiles("templates/toys.html"))
-    tmpl.Execute(w, toys)
+
+	tmpl := template.Must(template.ParseFiles("templates/toys.html"))
+
+	tmpl.Execute(w, toys)
+}
+
+func AddToCartHandler(w http.ResponseWriter, r *http.Request) {
+
+	toyID := r.URL.Query().Get("id")
+
+	for _, toy := range toys {
+
+		if toyID == "1" && toy.ID == 1 {
+			cart = append(cart, toy)
+		}
+
+		if toyID == "2" && toy.ID == 2 {
+			cart = append(cart, toy)
+		}
+
+		if toyID == "3" && toy.ID == 3 {
+			cart = append(cart, toy)
+		}
+
+		if toyID == "4" && toy.ID == 4 {
+			cart = append(cart, toy)
+		}
+	}
+
+	http.Redirect(w, r, "/cart", http.StatusSeeOther)
+}
+
+func CartHandler(w http.ResponseWriter, r *http.Request) {
+
+	tmpl := template.Must(template.ParseFiles("templates/cart.html"))
+
+	tmpl.Execute(w, cart)
+}
+
+func PlaceOrderHandler(w http.ResponseWriter, r *http.Request) {
+
+	cart = nil
+
+	w.Write([]byte("Order Placed Successfully"))
 }
 
 func AboutHandler(w http.ResponseWriter, r *http.Request) {
-    tmpl := template.Must(template.ParseFiles("templates/about.html"))
-    tmpl.Execute(w, nil)
+
+	tmpl := template.Must(template.ParseFiles("templates/about.html"))
+
+	tmpl.Execute(w, nil)
 }
 
 func ContactHandler(w http.ResponseWriter, r *http.Request) {
-    tmpl := template.Must(template.ParseFiles("templates/contact.html"))
-    tmpl.Execute(w, nil)
+
+	tmpl := template.Must(template.ParseFiles("templates/contact.html"))
+
+	tmpl.Execute(w, nil)
 }
--- main.go
+++ main.go
@@ -1,22 +1,31 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 
-    "go-web-app/handlers"
+	"go-web-app/handlers"
 )
 
 func main() {
 
-    fs := http.FileServer(http.Dir("./static"))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
+	// Static files
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-    http.HandleFunc("/", handlers.HomeHandler)
-    http.HandleFunc("/toys", handlers.ToysHandler)
-    http.HandleFunc("/about", handlers.AboutHandler)
-    http.HandleFunc("/contact", handlers.ContactHandler)
+	// Routes
+	http.HandleFunc("/", handlers.HomeHandler)
+	http.HandleFunc("/toys", handlers.ToysHandler)
+	http.HandleFunc("/cart", handlers.CartHandler)
+	http.HandleFunc("/add-to-cart", handlers.AddToCartHandler)
+	http.HandleFunc("/place-order", handlers.PlaceOrderHandler)
+	http.HandleFunc("/about", handlers.AboutHandler)
+	http.HandleFunc("/contact", handlers.ContactHandler)
 
-    log.Println("Server started on :8080")
-    http.ListenAndServe(":8080", nil)
+	log.Println("Toy Shop Server started on :8080")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
